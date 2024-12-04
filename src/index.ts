e0a2439a16e578a6c8f941e2bf27704c57ffbcfb
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-04 21:05:35 +0600

    updated some code

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-    // allowedHeaders: [""],
+    allowedHeaders: ["Authorization", "authToken", "Content-Type"],
     exposedHeaders: ["authToken"],
   })
 );
