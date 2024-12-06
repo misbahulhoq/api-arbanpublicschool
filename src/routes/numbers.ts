1b7f1d62be7687a3ffa09eeae0275d937be758de
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-06 12:58:28 +0600

    created single students number get method

--- src/routes/numbers.ts
+++ src/routes/numbers.ts
@@ -51,6 +51,13 @@ numbersRouter.get(
   }
 );
 
+numbersRouter.get("/:uid", verifyUser, async (req: Request, res: Response) => {
+  console.log(req.params.uid);
+  const number = await Num.findOne({ uid: req.params.uid });
+  console.log(number);
+  res.send(number);
+});
+
 numbersRouter.put(
   "/:uid",
   verifyUser,
