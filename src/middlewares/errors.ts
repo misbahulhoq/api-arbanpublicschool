91ed9c586e855a907a416439d5335a01e7e7c0f6
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-20 18:42:30 +0600

    errors will be sent to my email now

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -6,6 +6,7 @@ import type {
 } from "express";
 import { envValidator } from "../utils/envValidator";
 import winston, { createLogger } from "winston";
+import { contactEmailTransporter } from "../utils/emailTransport";
 const { combine, timestamp, json } = winston.format;
 const requiredEnv = [
   "jwtPrivateKey",
@@ -42,6 +43,32 @@ export const envValidatorMiddleware = (
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   logger.error(err.message);
+  contactEmailTransporter.sendMail({
+    from: process.env.contact_email,
+    to: ["extraordinarymisbah@gmail.com"],
+    subject: "An error happened in the server of Arban Public School Api",
+    html: `
+    <body style="font-family: Arial, sans-serif; background-color: #f8f9fa; padding: 20px; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+
+    <div style="
+        max-width: 500px; 
+        width: 100%; 
+        background-color: #fff; 
+        border-left: 5px solid #e74c3c; 
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); 
+        border-radius: 8px; 
+        padding: 20px; 
+        text-align: center;
+    ">
+        <h1 style="color: #e74c3c; font-size: 24px; margin: 0 0 10px;">⚠ Error Occurred</h1>
+        <p style="color: #333; font-size: 16px; line-height: 1.5; margin: 0 0 20px;">
+            ${err.message}
+        </p>
+    </div>
+
+</body>
+    `,
+  });
 };
 
 export default globalErrorHandler;
